a7ff110704fac17b2d97309e207c17c950ce8a68
izaac.morozetti.barboza2006 <izaac.morozetti.barboza2006@gmail.com>
2025-09-03 14:56:12 +0000

    Melhorando js, html e css

--- js/main.js
+++ js/main.js
@@ -60,4 +60,24 @@
     texto.classList.add('laser-active');
     setTimeout(() => texto.classList.remove('laser-active'), 300);
   });
+  document.querySelectorAll('.post').forEach(card => {
+    card.addEventListener('mousemove', e => {
+      const rect = card.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      const centerX = rect.width / 2;
+      const centerY = rect.height / 2;
+  
+      // cálculo de rotação baseado na posição do mouse
+      const rotateX = ((y - centerY) / centerY) * 8; 
+      const rotateY = ((x - centerX) / centerX) * 8;
+  
+      card.style.transform = `scale(1.08) rotateX(${-rotateX}deg) rotateY(${rotateY}deg)`;
+    });
+  
+    card.addEventListener('mouseleave', () => {
+      // volta ao estado normal suavemente
+      card.style.transform = "scale(1) rotateX(0) rotateY(0)";
+    });
+  });
   
